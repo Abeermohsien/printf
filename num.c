275de98d010de73f363e53c6cbcf5b136a1e5ac6
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-17 03:51:17 -0700

    ll

--- num.c
+++ num.c
@@ -24,49 +24,49 @@ int _strlen(char *s)
 }
 /**
  * print_number - prints numbber
- * @s: string
+ * @str: string
  * @params: param
  * Return: int
  */
-int print_number(char *s, params_t *params)
+int print_number(char *str, params_t *params)
 {
-	unsigned int n = _strlen(s);
-	int num = (!params->unsign && *s == '-');
+	unsigned int n = _strlen(str);
+	int num = (!params->unsign && *str == '-');
 
-	if (!params->precision && *s == '0' && !s[1])
-		s = "";
+	if (!params->precision && *str == '0' && !str[1])
+		str = "";
 	if (num)
 	{
-		s++;
+		str++;
 		n--;
 	}
 	if (params->precision != UINT_MAX)
 			while (n++ < params->precision)
-			*--s = '0';
+			*--str = '0';
 	if (num)
-	*--s = '-';
+	*--str = '-';
 
 	if  (!params->minus_flag)
-	return (print_number_right_shift(s, params));
+	return (print_number_right_shift(str, params));
 	else
-	return (print_number_left_shift(s, params));
+	return (print_number_left_shift(str, params));
 }
 /**
  * print_number_right_shift - print_number right shift
- * @s: string
+ * @str: string
  * @params: param
  * Return: int
  */
-int print_number_right_shift(char *s, params_t *params)
+int print_number_right_shift(char *str, params_t *params)
 {
-unsigned int n = 0, n1, n2, i = _strlen(s);
+unsigned int n = 0, n1, n2, i = _strlen(str);
 char padchar = ' ';
 
 if (params->zero_flag && !params->minus_flag)
 	padchar = '0';
-n1 = n2 = (!params->unsign && *s == '-');
+n1 = n2 = (!params->unsign && *str == '-');
 		if (n1 && i < params->width && padchar == '0' && !params->minus_flag)
-			s++;
+			str++;
 		else
 			n1 = 0;
 		if ((params->plus_flag && !n2) ||
@@ -88,25 +88,25 @@ n1 = n2 = (!params->unsign && *s == '-');
 		else if (!params->plus_flag && params->space_flag && !n2 &&
 				!params->unsign && !params->zero_flag)
 			n += _putchar(' ');
-		n += _puts(s);
+		n += _puts(str);
 		return (n);
 }
 /**
  * print_number_left_shift - print_number_right_shift
- * @s: string
+ * @str: string
  * @params: arg
  * Return: int
  */
-int print_number_left_shift(char *s, params_t *params)
+int print_number_left_shift(char *str, params_t *params)
 {
-	unsigned int n = 0, n1, n2, i = _strlen(s);
+	unsigned int n = 0, n1, n2, i = _strlen(str);
 	char padchar = ' ';
 
 	if (params->zero_flag && !params->minus_flag)
 		padchar = '0';
-	n1 = n2 = (!params->unsign && *s == ' ');
+	n1 = n2 = (!params->unsign && *str == ' ');
 	if (n1 && i < params->width && padchar == '0' && !params->minus_flag)
-		s++;
+		str++;
 	else
 		n1 = 0;
 
@@ -114,7 +114,7 @@ int print_number_left_shift(char *s, params_t *params)
 		n += _putchar('+'), i++;
 	else if (params->space_flag && !n2 && !params->unsign)
 		n += _putchar(' '), i++;
-	n += _puts(s);
+	n += _puts(str);
 	while (i++ < params->width)
 		n += _putchar(padchar);
 	return (n);
